04c836d86a39bdd30364b5b657701b6ca53f58ae
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-01 17:38:26 +0300

    Create Rails_Command_Guide.py

---
+++ Rails_Command_Guide.py
@@ -0,0 +1,23 @@
+Regarding tutorials on Rails commands and their usage, there are several resources available that provide comprehensive guides.
+Here are a few recommended sources:
+
+Ruby on Rails Tutorial by Michael Hartl: This popular online book provides an in-depth tutorial on building a 
+  Ruby on Rails application from scratch. It covers essential Rails commands and their usage in a tutorial format. 
+  Access the Ruby on Rails Tutorial: https://www.railstutorial.org/
+      Or access His youtube channel at Learn Enough or  https://www.youtube.com/playlist?list=PLdo16AWp70d1H4dlRSGSC8Sv83BMYPuAD
+      
+  
+Rails Command Line Cheat Sheet by Daniel Kehoe: This cheat sheet provides a quick reference for commonly used
+  Rails commands and their descriptions. It includes code samples and explanations for each command.
+  View the Rails Command Line Cheat Sheet: https://railscheatsheet.com/
+  
+  
+  
+  
+Rails Guides: The official Rails guides cover a wide range of topics, including the command-line tools and generators 
+  provided by Rails. You can find detailed explanations and code samples on how to use various Rails commands. 
+  Visit the Rails Guides website: https://guides.rubyonrails.org/
+
+
+
+These resources should help you understand and explore the different commands available in Rails and their functionalities.
