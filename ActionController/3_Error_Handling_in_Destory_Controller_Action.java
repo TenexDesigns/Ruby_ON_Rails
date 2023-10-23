9f1b018c35b8cc339b4a7345f576f9cc744f5568
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-23 14:34:12 +0300

    Create 3_Error_Handling_in_Destory_Controller_Action.java

---
+++ ActionController/3_Error_Handling_in_Destory_Controller_Action.java
@@ -0,0 +1,29 @@
+In Rails, you can handle exceptions in your controllers using `begin`/`rescue` blocks. Here's an example of how you might handle exceptions in the `destroy` action of a controller:
+
+```ruby
+class UsersController < ApplicationController
+  # DELETE /users/:id
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: { message: 'User successfully deleted' }, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: 'User not found' }, status: :not_found
+    rescue => e
+      render json: { error: 'Failed to delete User' }, status: :unprocessable_entity
+    end
+  end
+end
+```
+
+In this example, we're using a `begin`/`rescue` block to handle exceptions when trying to find and destroy a user.
+
+- If the user is found and successfully destroyed, we return a success message with a status of `:ok` (HTTP 200).
+- If the user is not found (`ActiveRecord::RecordNotFound` is raised), we return an error message with a status of `:not_found` (HTTP 404).
+- If any other error occurs, we return a generic error message with a status of `:unprocessable_entity` (HTTP 422).
+
+This is a simple example and in a real-world application, you might want to handle different types of errors differently. For example, if the error is due to a violation of a database constraint, you might want to return a different error message and status code.
+
+
+  ....
