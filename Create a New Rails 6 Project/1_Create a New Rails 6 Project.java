8f8da55598eb527d0a0f626d31d81f7968217a21
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-30 14:56:49 +0300

    Update 1_Create a New Rails 6 Project.java

--- Create a New Rails 6 Project/1_Create a New Rails 6 Project.java
+++ Create a New Rails 6 Project/1_Create a New Rails 6 Project.java
@@ -4,7 +4,7 @@ If you're starting a new project and want to use Rails 6, you can create a new R
 
 bash
 Copy code
-rails _6.0.4_ new my_app
+rails _6.0.4_ new my_app --api -d postgresql
 This command creates a new Rails application using version 6.0.4.
 
 Modifying the Rails version to an older major version is not recommended because it may lead to compatibility issues and require changes in your application code. It's better to specify a specific minor version of Rails 6 or lock the version to ensure your application remains stable.
