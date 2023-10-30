7dc9d3d1867b2d15ede551bfd574c855cfe822c8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-30 11:57:27 +0300

    Create 1_Create a New Rails 6 Project.java

---
+++ Create a New Rails 6 Project/1_Create a New Rails 6 Project.java
@@ -0,0 +1,15 @@
+Create a New Rails 6 Project:
+
+If you're starting a new project and want to use Rails 6, you can create a new Rails 6 project explicitly by running:
+
+bash
+Copy code
+rails _6.0.4_ new my_app
+This command creates a new Rails application using version 6.0.4.
+
+Modifying the Rails version to an older major version is not recommended because it may lead to compatibility issues and require changes in your application code. It's better to specify a specific minor version of Rails 6 or lock the version to ensure your application remains stable.
+
+
+
+
+  ...
