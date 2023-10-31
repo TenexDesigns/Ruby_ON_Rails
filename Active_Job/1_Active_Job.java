249dc84d1c2addb93fb90ef8e8cdcc812b1435c8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-31 16:19:52 +0300

    Update 1_Active_Job.java

--- Active_Job/1_Active_Job.java
+++ Active_Job/1_Active_Job.java
@@ -39,3 +39,50 @@ Remember that you can switch between different background job processing librari
 
 
   ....
+   -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+   In Rails, you can run code in the background using a feature called "background jobs." One of the primary tools for working with background jobs in Rails is Active Job. Active Job provides a unified interface for creating, queuing, and executing background tasks, while allowing you to use various background job processing libraries, such as Sidekiq, Resque, Delayed Job, and others, as backends.
+
+Here's how you can use Active Job to run code in the background in Rails:
+
+1. **Create a Job Class**: Start by creating a job class that inherits from `ActiveJob::Base`. This class will encapsulate the code you want to run in the background. For example:
+
+   ```ruby
+   class MyBackgroundJob < ApplicationJob
+     queue_as :default
+
+     def perform(*args)
+       # Your background job code goes here
+     end
+   end
+   ```
+
+2. **Queue the Job**: In your application code, when you want to run this code in the background, you can enqueue the job like this:
+
+   ```ruby
+   MyBackgroundJob.perform_later(arg1, arg2)
+   ```
+
+   The `perform_later` method enqueues the job with the specified arguments. The job will be executed by your chosen background job processing library.
+
+3. **Choose a Background Processing Library**: You need to decide which background job processing library you want to use. Popular choices include Sidekiq, Resque, and Delayed Job. You'll need to configure your Rails application to use your chosen library. Each library has its own configuration steps.
+
+4. **Start the Background Job Worker**: You'll also need to run the worker process associated with your chosen background job processing library. This worker process will pull jobs from the queue and execute them in the background.
+
+   - For Sidekiq: Run `bundle exec sidekiq` to start the Sidekiq worker process.
+   - For Resque: Run `QUEUE=* bundle exec rake resque:work` to start a Resque worker.
+   - For Delayed Job: Run `bin/delayed_job start` to start the Delayed Job worker.
+
+The background job processing library you choose will handle the execution of the background job, and it may offer additional features like job retries, error handling, and job prioritization.
+
+Using background jobs is helpful when you have tasks that are time-consuming or might block the main request-response cycle, such as sending emails, processing uploaded files, or performing other tasks that don't need to be completed immediately.
+
+Remember that you can switch between different background job processing libraries without changing your job code, thanks to Active Job's abstraction. This provides flexibility and scalability as your application grows.
+
+
+
+
+
+
+   ......
