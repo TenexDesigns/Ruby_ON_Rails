1ea6963a3eded0e1fee083994f13588c55a3f548
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-02 11:05:36 +0300

    Create 1_Data_that_Can_Be_Generated.java

---
+++ Faker gem to generate seed data/Data_that_Can_Be_Generated/1_Data_that_Can_Be_Generated.java
@@ -0,0 +1,43 @@
+Sure, here is a list of fake data that can be generated using the Faker gem:
+
+* Names (first, last, full)
+* Addresses (including street address, city, state, zip code, country)
+* Phone numbers (including landline and mobile numbers)
+* Email addresses
+* Websites
+* Dates and times (including birthdates, anniversaries, and other events)
+* Credit card numbers
+* Currency values
+* Lorem ipsum text
+* Usernames
+* Passwords
+* IP addresses
+* Job titles
+* Company names
+* Product names
+* Prices
+* Descriptions
+* Reviews
+* Locations (lat/long coordinates)
+* Colors
+* Animals
+* Countries
+* Cities
+* Languages
+* And much more!
+
+The Faker gem also provides generators for specific types of data, such as:
+
+* Internet data (IP addresses, email addresses, websites)
+* Commerce data (product names, prices, descriptions)
+* Company data (company names, addresses, phone numbers)
+* Location data (lat/long coordinates, countries, cities)
+* Vehicle data (vehicle make, model, year)
+* And much more!
+
+To see a full list of the data that Faker can generate, please refer to the Faker documentation.
+
+I hope this helps!
+
+
+  
