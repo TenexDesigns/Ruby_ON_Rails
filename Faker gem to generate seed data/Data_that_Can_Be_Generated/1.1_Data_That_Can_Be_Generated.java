90afb428fe1a8fc35dc56476466dde9e002ff955
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-02 11:06:21 +0300

    Create 1.1_Data_That_Can_Be_Generated.java

---
+++ Faker gem to generate seed data/Data_that_Can_Be_Generated/1.1_Data_That_Can_Be_Generated.java
@@ -0,0 +1,33 @@
+The Faker gem can generate a wide variety of fake data. Here's a list of some of the types of data it can generate:
+
+1. **Names**: It can generate fake names using `Faker::Name.name`. 
+
+2. **Email Addresses**: It can generate fake email addresses using `Faker::Internet.email`.
+
+3. **Addresses**: It can generate fake addresses using `Faker::Address.street_address`.
+
+4. **Phone Numbers**: It can generate fake phone numbers using `Faker::PhoneNumber.phone_number`.
+
+5. **Text**: It can generate fake sentences or paragraphs using `Faker::Lorem.sentence` or `Faker::Lorem.paragraph`.
+
+6. **Dates**: It can generate fake past or future dates using `Faker::Date.backward` or `Faker::Date.forward`.
+
+7. **URLs**: It can generate fake URLs using `Faker::Internet.url`.
+
+8. **Job Titles**: It can generate fake job titles using `Faker::Job.title`.
+
+9. **Colors**: It can generate fake color names using `Faker::Color.color_name`.
+
+10. **Credit Card Numbers**: It can generate fake credit card numbers using `Faker::Finance.credit_card`.
+
+11. **File Names**: It can generate fake file names and extensions using `Faker::File.file_name`.
+
+12. **Usernames**: It can generate fake usernames using `Faker::Internet.username`.
+
+13. **Passwords**: It can generate fake passwords using `Faker::Internet.password`.
+
+14. **Business Types**: It can generate fake business types using `Faker::Company.type`.
+
+15. **Currency**: It can generate fake currency codes using `Faker::Currency.code`.
+
+And many more. You can explore the [Faker documentation](https://github.com/faker-ruby/faker) for a complete list of available methods [Source 0](https://github.com/faker-ruby/faker).
