d7d97bdf35fee30dce96367bb75313ee00f3d8ce
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-02 10:57:41 +0300

    Create 1_Clients for Graphql.java

---
+++ GraphQL/Clients for Graphql/1_Clients for Graphql.java
@@ -0,0 +1,17 @@
+To test RESTful APIs, Postman is a popular and powerful tool. When working with GraphQL APIs, you can use similar tools that are specifically designed to work with GraphQL. Here are some popular tools for testing GraphQL APIs:
+
+1. **Altair:** Altair is a GraphQL client for desktop that allows you to write and test GraphQL queries and mutations. It's available as a browser extension and as a standalone desktop application.
+
+2. **Thunder Client:** Thunder Client is a VS Code extension that supports REST and GraphQL. You can write and execute GraphQL queries and mutations directly within your code editor.
+
+3. **Postman
+Postman is another API development platform that is quite popular among developers. Much like Insomnia, Postman isn't specifically built for GraphQL, rather better suited for REST APIs. However, the support for GraphQL has become a bit more sophisticated and robust. Its auto-generated API documentation is one of its strongest features. It also lets you connect to mock servers for simulating endpoints which allows developers to test different responses without running an actual backend.
+
+4. **Hoppscotch): Postwoman is an open-source, easy-to-use API request builder that supports GraphQL. It allows you to send GraphQL queries and mutations and view the responses. It's available as a web application and desktop application.
+
+  
+These tools offer various features and are suitable for different preferences and workflows. You can choose the one that best fits your needs and integrates well with your development environment.
+
+
+
+  
