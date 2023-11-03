7e1561b8847511ea6fc761bd720557142cdaa414
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-03 22:14:39 +0300

    Create 3_Micro_Service_Books.java

---
+++ Microservices/Books/3_Micro_Service_Books.java
@@ -0,0 +1,38 @@
+Here are some books and learning resources on microservices with Rails, deployment, and projects, and Kubernetes with Rails and projects with Rails:
+
+**Microservices with Rails**
+
+* **Microservices with Rails: Building Scalable and Reliable Software** by Sam Newman
+* **Microservices in Action** by Sebastien Lambla
+* **Microservices: From Design to Deployment** by Chris Richardson
+* **Microservices Design Patterns: A Hands-On Guide** by Sanda Manu
+* **Microservices with Node.js and React: Building a Real-World Application** by Flavio Torres
+
+**Deployment**
+
+* **Deploying Rails Applications: A Comprehensive Guide to Getting Your App Online** by Sam Saffron
+* **Deploying Rails with Docker, Kubernetes and ECS** by Pablo Acuña
+* **Infrastructure as Code: Managing Servers with Terraform, Ansible, Chef, and Puppet** by Kief Morris
+* **Production-Ready Microservices: Strategies for Building and Running Scalable Distributed Systems** by Susan Fowler
+* **Continuous Delivery: Reliable Software Releases Through Build, Test, and Deployment Automation** by Jez Humble and David Farley
+
+**Projects with Rails**
+
+* **Rails Tutorial: Learn Rails by Example** by Michael Hartl
+* **Agile Web Development with Rails 7** by Sam Ruby, Dave Thomas, and David Heinemeier Hansson
+* **Rails 7 in Action** by Ryan Bigg
+* **The Rails 7 Way** by Lucas Manske, Obie Fernandez, and Aaron Patterson
+* **Building Web Applications with Rails: A Pragmatic Guide** by Ryan Singer
+
+**Kubernetes with Rails**
+
+* **Kubernetes for Developers: A Practical Guide** by Michael Hausenblas
+* **Kubernetes in Action** by Marko Luksa and Kelsey Hightower
+* **Kubernetes for Beginners: A Step-by-Step Guide** by Grant Ingersoll and Kurt Ingersoll
+* **Kubernetes: Up and Running: Dive into the Future of Infrastructure** by Kelsey Hightower, Brendan Burns, and Joe Beda
+* **Kubernetes for Rails Developers: Building and Deploying Modern Web Applications** by Samet Yilmaz
+
+I hope this helps!
+
+
+  ...
