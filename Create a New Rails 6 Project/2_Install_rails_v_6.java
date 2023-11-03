078b1d012bf00bbf158803270500c0de6361893d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-03 14:38:59 +0300

    Create 2_Install_rails_v_6.java

---
+++ Create a New Rails 6 Project/2_Install_rails_v_6.java
@@ -0,0 +1,38 @@
+To install Rails version 6.1, you'll need to use RubyGems, which is the package manager for Ruby. Here are the steps to install Rails 6.1:
+
+1. **Install Ruby**: Ensure you have Ruby installed on your system. You can check your current Ruby version by running:
+
+   ```bash
+   ruby -v
+   ```
+
+   If Ruby is not installed, youll need to install it. You can use a version manager like RVM or rbenv, or install it directly from your system's package manager (e.g., `apt`, `yum`, `brew`).
+
+2. **Install Rails**: Once you have Ruby installed, you can install Rails using RubyGems. Open your terminal and run the following command:
+
+   ```bash
+   gem install rails -v 6.1
+   ```
+
+   This command tells RubyGems to install Rails version 6.1.
+
+3. **Verify Installation**: After the installation is complete, you can verify that Rails 6.1 is installed by running:
+
+   ```bash
+   rails -v
+   ```
+
+   This should display the version number, indicating that Rails 6.1 is successfully installed.
+
+4. **Optional: Create a New Rails Application**: If you want to create a new Rails application using version 6.1, you can run:
+
+   ```bash
+   rails new myapp
+   ```
+
+   Replace `myapp` with the name of your application. This command will generate a new Rails project using Rails 6.1.
+
+That's it! You now have Rails version 6.1 installed on your system. You can start developing your Rails applications using this version.
+
+
+  
